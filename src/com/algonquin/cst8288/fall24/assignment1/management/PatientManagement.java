5be28efaa6901d0fd2abf19c905c07861d14cb54
wang1003 <wang1003@algonquinlive.com>
2024-09-28 19:24:16 -0400

    Refactored PatientManagement to adhere to DIP

--- src/com/algonquin/cst8288/fall24/assignment1/management/PatientManagement.java
+++ src/com/algonquin/cst8288/fall24/assignment1/management/PatientManagement.java
@@ -8,88 +8,91 @@ import java.time.Period;
 import java.time.format.DateTimeFormatter;
 
 import com.algonquin.cst8288.fall24.assignment1.patient.Patient;
-import com.algonquin.cst8288.fall24.assignment1.prescription.OnlinePrescriptionService;
 import com.algonquin.cst8288.fall24.assignment1.prescription.PrescriptionService;
-import com.algonquin.cst8288.fall24.assignment1.treatment.SurgeryTreatmentPlan;
 import com.algonquin.cst8288.fall24.assignment1.treatment.TreatmentPlan;
 import com.alognquin.cst8288.fall24.assignment1.Constants;
 
-
 /**
- * The patient management class  used by Physicians to perform multiple tasks.
- * 
+ * The patient management class used by Physicians to perform multiple tasks.
+ *
  */
-
 public class PatientManagement {
 
-	
+    private final TreatmentPlan treatmentPlan;
+    private final PrescriptionService prescriptionService;
 
+    // Constructor injection TreatmentPlan
+    public PatientManagement(TreatmentPlan treatmentPlan) {
+        this.treatmentPlan = treatmentPlan;
+        this.prescriptionService = null;
+    }
 
+    // Constructor injection PrescriptionService
+    public PatientManagement(PrescriptionService prescriptionService) {
+        this.prescriptionService = prescriptionService;
+        this.treatmentPlan = null;
+    }
 
-	
+    // Constructor injection TreatmentPlan & PrescriptionService
+    public PatientManagement(TreatmentPlan treatmentPlan, PrescriptionService prescriptionService) {
+        this.treatmentPlan = treatmentPlan;
+        this.prescriptionService = prescriptionService;
+    }
 
+    /**
+     * Create treatment plan
+     *
+     * @param patient
+     */
+    public void createTreatmentPlan(Patient patient) {
+        patient.setAge(this.calulatePatientAge(patient.getDateOfBirth()));
+        patient.setLifeStage(this.determineLifeStage(patient.getAge()));
+        treatmentPlan.createTreatmentPlan(patient);
 
-	/**
-	 * Calculate patient age based on date of birth
-	 * 
-	 * @param dob
-	 * @return
-	 */
-	public long calulatePatientAge(String dob) {
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+    }
+
+    /**
+     * Create prescription
+     *
+     * @param patient
+     * @param filename
+     * @throws IOException
+     */
+    public void generatePrescription(Patient patient, String filename) throws IOException {
+        BufferedWriter bwriter;
+        bwriter = new BufferedWriter(new FileWriter(filename));
+        bwriter.write(prescriptionService.generatePrescription(patient));
+        bwriter.close();
+    }
+
+    /**
+     * Calculate patient age based on date of birth
+     *
+     * @param dob
+     * @return
+     */
+    public long calulatePatientAge(String dob) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
         LocalDate dateOfBirth = LocalDate.parse(dob, formatter);
-		
+
         LocalDate currentDate = LocalDate.now();
         long age = Period.between(dateOfBirth, currentDate).getYears();
-        
+
         return age;
-      
-	}
-	
-	/**
-	 * Determine life stage based on age
-	 * 
-	 * @param age
-	 * @return
-	 */
-	public String determineLifeStage(long age) {
-		
-		return (age <= 6) ? Constants.CHILD : 
-     	   (age <= 18) ? Constants.YOUTH : 
-     		  Constants.ADULT;   
-	}
-	
 
-	/**
-	 * Create treatment plan
-	 * 
-	 * @param patient
-	 */
-	public void createTreatmentPlan(Patient patient){
-		TreatmentPlan treatment = new SurgeryTreatmentPlan();
-		patient.setAge(this.calulatePatientAge(patient.getDateOfBirth()));
-		patient.setLifeStage(this.determineLifeStage(patient.getAge()));
-		treatment.createTreatmentPlan(patient);
-		
-	}
+    }
 
+    /**
+     * Determine life stage based on age
+     *
+     * @param age
+     * @return
+     */
+    public String determineLifeStage(long age) {
 
-	/**
-	 * Create prescription
-	 * 
-	 * @param patient
-	 * @param filename
-	 * @throws IOException
-	 */
-	public void generatePrescription(Patient patient, String filename) throws IOException {
-		PrescriptionService prescriptionService = new OnlinePrescriptionService();
-		BufferedWriter bwriter;
-		bwriter = new BufferedWriter(new FileWriter(filename));	
-		bwriter.write(prescriptionService.generatePrescription(patient));
-		bwriter.close();
-	}
+        return (age <= 6) ? Constants.CHILD
+                : (age <= 18) ? Constants.YOUTH
+                        : Constants.ADULT;
+    }
 
 }
-
-
-
